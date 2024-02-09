4783bab3684f1d84b236031b552a2f8aec55c169
Walter Park <neodust223@gmail.com>
2024-02-09 20:22:21 +0900

    Update oop_test.py

--- oop_test.py
+++ oop_test.py
@@ -8,25 +8,25 @@ WORDS = []
 PHRASES = {
     "class %%%(%%%):":
         "Make a class named %%% that is-a %%%.",
-    "class %%%(object):\n\tdef__init__(self, ***)":
+    "class %%%(object):\n\tdef __init__(self, ***)":
         "class %%% has-a __init__ that takes self and *** params.",
     "class %%%(object):\n\tdef ***(self, @@@)":
         "class %%% has-a function *** that takes self and @@@ params.",
-    "*** = %%%():":
+    "*** = %%%()":
         "Set *** to an instance of class %%%.",
     "***.***(@@@)":
         "From *** get the *** function, call it with params self, @@@",
     "***.*** = '***'":
-        "From *** get the *** attribute and set it to '***''."
+        "From *** get the *** attribute and set it to '***'."
 }
 
-# do they want to drill pharases first
+# Do they want to drill phrases first?
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 else:
     PHRASE_FIRST = False
 
-# load up the words from the website
+# Load up the words from the website
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(str(word.strip(), encoding="utf-8"))
 
@@ -38,21 +38,21 @@ def convert(snippet, phrase):
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
-        param_count = random.randint(1,3)
+        param_count = random.randint(1, 3)
         param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
         result = sentence[:]
 
-        # fake class names
+        # Fake class names
         for word in class_names:
             result = result.replace("%%%", word, 1)
 
-        # fake other names
+        # Fake other names
         for word in other_names:
             result = result.replace("***", word, 1)
 
-        # fake parameter lists
+        # Fake parameter lists
         for word in param_names:
             result = result.replace("@@@", word, 1)
 
@@ -60,7 +60,7 @@ def convert(snippet, phrase):
 
     return results
 
-# keep going until they hit CTRL-D
+# Keep going until they hit CTRL-D
 try:
     while True:
         snippets = list(PHRASES.keys())
@@ -71,7 +71,7 @@ try:
             question, answer = convert(snippet, phrase)
             if PHRASE_FIRST:
                 question, answer = answer, question
-            
+
             print(question)
 
             input("< ")
