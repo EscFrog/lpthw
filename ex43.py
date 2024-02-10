98e90380fd412245d3a2b9482d0cf0ddb549286a
Walter Park <neodust223@gmail.com>
2024-02-10 23:17:53 +0900

    Update ex43.py

--- ex43.py
+++ ex43.py
@@ -28,6 +28,7 @@ class Engine(object):
     #be sure to print out the last scene
     current_scene.enter()
 
+
 class Death(Scene):
 
   quips = [
@@ -41,6 +42,7 @@ class Death(Scene):
   def enter(self):
     print(Death.quips[randint(0, len(self.quips)-1)])
     exit(1)
+
 
 class CentralCorridor(Scene):
 
@@ -85,16 +87,107 @@ class CentralCorridor(Scene):
       """))
       return 'death'
 
+    elif action == "tell a joke":
+      print(dedent("""
+        Lucky for you they made you learn Gothon insults in
+        the academy. You tell the one Gothon joke you know:
+        Lbhe zbgure vf fb sng, jura fur fvgf nebhaq qur ubhfr,
+        fur fvgf nbhaq qur ubhfr. The Gothon stops, tries move.
+        While he's laughing you run up and shoot him square in
+        the head putting him down, then jump through the
+        Weapon Amory door.
+        """))
+      return 'laser_weapon_armory'
+
+    else:
+      print("DOES NOT COMPUTE!")
+      return 'central_corridor'
+
 
 class LaserWeaponArmory(Scene):
 
   def enter(self):
-    pass
+    print(dedent("""
+      You do a dive roll into the Weapon Armory, crouch and scan
+      the room for more Gothons that might be hiding. It's dead
+      quiet, too quiet. You stand up and run to the far side of
+      the room and find the neutron bomb in its container.
+      There's a keypad lock on the box and you need the code to
+      get the bomb out. If you get the code wrong 10 times then
+      the lock closes forever and you can't get the bomb. The
+      code is 3 digits.
+      """))
+
+    code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
+    guess = input("[keypad]> ")
+    guesses = 0
+
+    while guess != code and guesses < 10:
+      print("BZZZZEDDD!")
+      guesses += 1
+      guess = input("[keypad]> ")
+
+    if guess == code:
+      print(dedent("""
+        The container clicks open and the seal breaks, letting
+        gas out. You grab the neutron bomb and run as fast as
+        you can to the bridge where you must place it in the
+        right spot.
+        """))
+      return 'the_bridge'
+    else:
+      print(dedent("""
+        The lock buzzes one last time and then you hear a
+        sickening melting sound as the mechanism is fused
+        together. You decide to sit there, and finally the
+        Gothons blow up the ship from their ship and you die.
+        """))
+      return 'death'
+
 
 class theBridge(Scene):
 
   def enter(self):
-    pass
+    print(dedent("""
+      You burst onto the Bridge with the netron destruct bomb
+      under your arm and surprise 5 Gothons who are trying to
+      take control of the ship. Each of them has an even uglier
+      clown costume than the last. They haven't pulled their
+      weapons out yet, as they see the active bomb under your
+      arm and don't want to set it off.
+      """))
+
+    action = input("> ")
+
+    if action == "throw the bomb":
+      print(dedent("""
+        In a panic you throw the bomb at the group of Gothons
+        and make a leap for the door. Right as you drop it a
+        Gothon shoots you right in the back killing you. As
+        you die you see another Gothon frantically try to
+        disarm the bomb. You die knowing they will probably
+        blow up when it goes off.
+        """))
+
+      return 'death'
+
+    elif action == "slowly place the bomb":
+      print(dedent("""
+        You point your blaster at the bomb under your arm and
+        the Gothons put their hands up and start to sweat.
+        You inch backward to the door, open it, and then
+        carefully place the bomb on the floor, pointing your
+        blaster at it. You then jump back through the door,
+        punch the close button and blast the lock so the
+        Gothons can't get out. Now that the bomb is placed
+        you run to the escape pod to get off this tin can.
+        """))
+
+      return 'escape_pod'
+    else:
+        print("DOES NOT COMPUTE!")
+        return "the_bridge"
+
 
 class EscapePod(Scene):
 
